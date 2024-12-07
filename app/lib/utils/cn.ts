ccd8591f7c35018ef0d78fafc64aebd20d05745c
sanaz niamati <93905446+sanazniamati@users.noreply.github.com>
2024-12-07 23:06:54 +0330

    bulid:add cn func to merg tailwind classes

---
+++ app/lib/utils/cn.ts
@@ -0,0 +1,9 @@
+/** @format */
+
+import clsx from "clsx";
+import { ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export const cn = (...classes: ClassValue[]) => {
+  return twMerge(clsx(classes.filter(Boolean)));
+};
