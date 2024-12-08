f252e317e13e7672a75c65bb5e9652230f0cf3d7
sanaz niamati <93905446+sanazniamati@users.noreply.github.com>
2024-12-08 21:36:36 +0330

    build:added Button component

--- app/lib/design/common/button/index.tsx
+++ app/lib/design/common/button/index.tsx
@@ -3,6 +3,7 @@
 import { FC } from "react";
 import { Props } from "./index.types";
 import { FaSpinner } from "react-icons/fa";
+import { cn } from "@/app/lib/utils/cn";
 
 const Button: FC<Props> = ({
   children,
@@ -11,10 +12,18 @@ const Button: FC<Props> = ({
   isOutline = false,
   leftIcon,
   rightIcon,
+  className,
   ...rest
 }) => {
+  const baseClasses =
+    "flex items-center justify-center font-medium rounded transition duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2";
+
+  const outlineClasses = isOutline ? "border-2 bg-transparent" : "";
+
+  const disabledClasses = isLoading ? "opacity-50 cursor-not-allowed" : "";
+
   return (
-    <button>
+    <button className={cn(baseClasses, outlineClasses, disabledClasses, className)}>
       {isLoading ? (
         <FaSpinner className="animate-spin text-lg" />
       ) : (
@@ -27,3 +36,5 @@ const Button: FC<Props> = ({
     </button>
   );
 };
+
+export default Button;
