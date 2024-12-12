832fd0c4d151b688f7d00c8c9e9ae1efb223e0d2
sanaz niamati <93905446+sanazniamati@users.noreply.github.com>
2024-12-12 08:55:25 +0330

    build:added Text component

---
+++ app/lib/design/common/text/index.tsx
@@ -0,0 +1,25 @@
+/** @format */
+
+import { cn } from "@/app/lib/utils/cn";
+import { Props } from "./index.types";
+
+const Text: React.FC<Props> = ({ as = "p", size = "md", weight = "normal", children, className }) => {
+  const Tag = as as keyof JSX.IntrinsicElements;
+
+  const baseClasses = "text-gray-800";
+
+  const sizeClasses = {
+    sm: "text-sm",
+    md: "text-base",
+    lg: "text-lg",
+  };
+
+  const weightClasses = {
+    normal: "font-normal",
+    semibold: "font-semibold",
+    bold: "font-bold",
+  };
+  return <Tag className={cn(baseClasses, sizeClasses[size], weightClasses[weight], className)}>{children}</Tag>;
+};
+
+export default Text;
---
+++ app/lib/design/common/text/index.types.ts
@@ -0,0 +1,9 @@
+/** @format */
+
+export interface Props {
+  as?: "p" | "div" | "span";
+  size?: "sm" | "md" | "lg";
+  weight?: "normal" | "bold" | "semibold";
+  className?: string;
+  children?: React.ReactNode;
+}
