7709d306cebb5e5429ab754f701fd272f5460918
sanaz niamati <93905446+sanazniamati@users.noreply.github.com>
2024-12-12 11:15:03 +0330

    build:added cart button

---
+++ app/lib/design/common/cart/index.tsx
@@ -0,0 +1,25 @@
+/** @format */
+
+import { cn } from "@/app/lib/utils/cn";
+import LinkContainer from "../link-container";
+import { CartProps } from "./index.types";
+import { FaShoppingCart } from "react-icons/fa";
+import Text from "../text";
+
+const Cart: React.FC<CartProps> = ({ itemCount = 5, className }) => {
+  return (
+    <LinkContainer href={"/cart"} classes={cn("relative cursor-pointer", className)}>
+      <FaShoppingCart size={24} className="text-white" />
+      {itemCount > 0 && (
+        <Text
+          as="span"
+          className="absolute right-0 top-0  bg-red-500 text-white text-xs font-bold rounded-full h-5 w-5 
+          flex items-center justify-center transform  translate-x-1/2 -translate-y-1/2"
+        >
+          {itemCount}
+        </Text>
+      )}
+    </LinkContainer>
+  );
+};
+export default Cart;
---
+++ app/lib/design/common/cart/index.types.ts
@@ -0,0 +1,6 @@
+/** @format */
+
+export interface CartProps {
+  itemCount?: number;
+  className?: string;
+}
